6ee6cdac85b2eaf43dbbe1fb238d9d67c92bda13
Angelo Moreira <angelo.moreira@gmail.com>
2022-04-28 17:32:09 +0100

    slices example

--- sum/sum.go
+++ sum/sum.go
@@ -7,3 +7,12 @@ func Sum(numbers []int) int {
 	}
 	return total
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
